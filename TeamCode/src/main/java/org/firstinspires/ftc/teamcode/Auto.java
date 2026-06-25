76aee3557f5cd4f7cf783b10156d37b64343aaf7
Chanu <crsabbineni225@gmail.com>
2026-06-25 14:48:11 -0400

    Merge pull request #1944 from FIRST-Tech-Challenge/20251231-104637-release-candidate
    FtcRobotController v11.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -1,18 +1,24 @@
 package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
 @Autonomous(name = "Auto", group = "OpMode")
 public class Auto extends OpMode {
     DcMotor motorFL;
+    DcMotor motorFR;
+    DcMotor motorBL;
+    DcMotor motorBR;
     @Override
     public void init() {
         motorFL = hardwareMap.get(DcMotor.class, "driveFL");
+        motorFR = hardwareMap.get(DcMotor.class, "driveFR");
+        motorBL = hardwareMap.get(DcMotor.class, "driveBL");
+        motorBR = hardwareMap.get(DcMotor.class, "driveBR");
     }
     @Override
     public void loop() {
-        motorFL.setPower(0.3);
         }
     }
