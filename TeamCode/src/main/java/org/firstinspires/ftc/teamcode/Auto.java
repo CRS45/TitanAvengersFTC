7861827240a34914e46a71aa77c6a200a8cd8453
Chanu <crsabbineni225@gmail.com>
2026-07-09 09:52:09 -0400

    Merge pull request #1944 from FIRST-Tech-Challenge/20251231-104637-release-candidate
    FtcRobotController v11.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto.java
@@ -3,21 +3,44 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @Autonomous(name = "Auto", group = "OpMode")
 public class Auto extends OpMode {
     DcMotor motorFL;
     DcMotor motorFR;
-    DcMotor motorBL;
-    DcMotor motorBR;
+    DcMotor motorRL;
+    DcMotor motorRR;
+    DcMotor intake;
+    DcMotor geckoWheel;
+    DcMotor sushiroll;
+    DcMotor flywheel;
+
     @Override
     public void init() {
         motorFL = hardwareMap.get(DcMotor.class, "driveFL");
         motorFR = hardwareMap.get(DcMotor.class, "driveFR");
-        motorBL = hardwareMap.get(DcMotor.class, "driveBL");
-        motorBR = hardwareMap.get(DcMotor.class, "driveBR");
+        motorRL = hardwareMap.get(DcMotor.class, "driveRL");
+        motorRR = hardwareMap.get(DcMotor.class, "driveRR");
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorRL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        geckoWheel = hardwareMap.get(DcMotor.class, "gecko_wheel");
+        sushiroll = hardwareMap.get(DcMotor.class, "sushi_roll");
+        flywheel = hardwareMap.get(DcMotor.class, "fly_wheel");
+        geckoWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        sushiroll.setDirection(DcMotorSimple.Direction.REVERSE);
     }
+
     @Override
     public void loop() {
-        }
+        intake.setPower(0.5);
+        geckoWheel.setPower(0.5);
+        sushiroll.setPower(0.5);
+        flywheel.setPower(0.5);
     }
+
+
+}
+
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drive.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp(name = "Basic TeleOp", group = "OpMode")
+public class Drive extends OpMode {
+    DcMotor motorFL;
+    DcMotor motorFR;
+    DcMotor motorBL;
+    DcMotor motorBR;
+    DcMotor intake;
+    DcMotor geckoWheel;
+    DcMotor sushiroll;
+    DcMotor flywheel;
+    @Override
+    public void init() {
+        motorFL = hardwareMap.get(DcMotor.class, "driveFL");
+        motorFR = hardwareMap.get(DcMotor.class, "driveFR");
+        motorBL = hardwareMap.get(DcMotor.class, "driveRL");
+        motorBR = hardwareMap.get(DcMotor.class, "driveRR");
+        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        intake = hardwareMap.get(DcMotor.class, "intake");
+        geckoWheel = hardwareMap.get(DcMotor.class, "gecko_wheel");
+        sushiroll = hardwareMap.get(DcMotor.class, "sushi_roll");
+        flywheel = hardwareMap.get(DcMotor.class, "fly_wheel");
+        geckoWheel.setDirection(DcMotorSimple.Direction.REVERSE);
+        sushiroll.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+    @Override
+    public void loop() {
+        ForwardBackward(gamepad1.left_stick_y);
+        if (gamepad1.left_trigger_pressed) {
+            intakeBall(0.5);
+        }
+        if (gamepad1.right_trigger_pressed) {
+            shootBall(0.5);
+        }
+    }
+
+    public void ForwardBackward(double power) {
+        motorFL.setPower(power);
+        motorFR.setPower(power);
+        motorBL.setPower(power);
+        motorBR.setPower(power);
+    }
+
+    public void intakeBall(double power) {
+        intake.setPower(power);
+        geckoWheel.setPower(power);
+        sushiroll.setPower(power);
+
+    }
+
+    public void shootBall(double power) {
+        flywheel.setPower(power);
+    }
+}
