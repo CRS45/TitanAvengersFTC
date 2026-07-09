f51fdfad1361ecef6dca3b22e9cee92bd945ac40
Chanu <crsabbineni225@gmail.com>
2026-07-09 11:53:27 -0400

    Merge pull request #1944 from FIRST-Tech-Challenge/20251231-104637-release-candidate
    FtcRobotController v11.1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/limelight.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -8,10 +9,16 @@ import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.hardware.IMU;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 
-@TeleOp(name = "Limelight AprilTag", group = "OpMode")
+@Autonomous(name = "Limelight AprilTag", group = "OpMode")
 public class limelight extends OpMode {
     private Limelight3A limelight;
+
+    private double CAMERA_HEIGHT=0.368;
+    private double CAMERA_ANGLE=0;
+    private double GOAL_HEIGHT=0.45085;
+    private double distance=0.508;
     private IMU imu;
     DcMotor motorOne;
     @Override
@@ -42,12 +49,23 @@ public class limelight extends OpMode {
         limelight.updateRobotOrientation(orientation.getYaw());
         LLResult llResult= limelight.getLatestResult();
         if (llResult != null && llResult.isValid()) {
+            distance=getDistance(llResult.getTy());
             Pose3D botpose = llResult.getBotpose_MT2();
             telemetry.addData("Target X", llResult.getTx());
             telemetry.addData("Target Y", llResult.getTy());
             telemetry.addData("Target Area", llResult.getTa());
             telemetry.addData("Bot Pose", botpose.toString());
+            telemetry.addData("Distance", distance);
+        } else {
+            telemetry.addData("No Valid Target", "Found");
         }
+    }
+
+    public double getDistance(double ty) {
+        double AngleToTarget = CAMERA_ANGLE+ty;
+        double heightDifference=GOAL_HEIGHT-CAMERA_HEIGHT;
+
+        return heightDifference / Math.tan(Math.toRadians(AngleToTarget));
     }
 
 }
